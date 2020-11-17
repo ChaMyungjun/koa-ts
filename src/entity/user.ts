65785a7a960b608dd3ae6643c13f8b1bad11acc1
ChaMyungjun <acg70159@gmail.com>
2020-11-17 20:35:05 +0900

    login & register setting

--- src/entity/user.ts
+++ src/entity/user.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Entity, Column, PrimaryGeneratedColumn, BaseEntity } from "typeorm";
 import { Length, IsEmail } from "class-validator";
+import crypto from "crypto";
 
 @Entity()
 export class User extends BaseEntity {
@@ -35,10 +36,24 @@ export const userSchema = {
   email: {
     type: "string",
     required: true,
-    example: "avileslopez.javier@gmail.com",
+    example: "avileslopezjavier@gmail.com",
   },
   password: {
     type: "string",
     required: true,
   },
 };
+
+//password hashed before input db
+export function hashedPassword(passowrd: any) {
+  return crypto
+    .createHmac("sha256", process.env.SECRET_KEY)
+    .update(passowrd)
+    .digest("hex");
+}
+
+//compare login input password & db.password
+export function comparePassword(password: any, passwordConfirm: any) {
+  const hahsed = hashedPassword(passwordConfirm);
+  return password === hahsed;
+}
--- src/protectedRoutes.ts
+++ src/protectedRoutes.ts
@@ -1,15 +1,21 @@
 import { SwaggerRouter } from "koa-swagger-decorator";
+import passport from "passport";
 import { user } from "./controller";
 
 const protectedRouter = new SwaggerRouter();
 
 // USER ROUTES
-protectedRouter.get("/users", user.getUsers);
-protectedRouter.get("/users/:id", user.getUser);
-protectedRouter.post("/users", user.createUser);
-protectedRouter.put("/users/:id", user.updateUser);
-protectedRouter.delete("/users/:id", user.deleteUser);
-protectedRouter.delete("/testusers", user.deleteTestUsers);
+protectedRouter.get("/users", user.getUsers); //All Member find
+protectedRouter.post("/login/local", user.getUser); //Login
+protectedRouter.post("/register/local", user.createUser); //register
+protectedRouter.put("/users/:id", user.updateUser); //modify
+protectedRouter.delete("/users/:id", user.deleteUser); //specified member delete
+protectedRouter.delete("/testusers", user.deleteTestUsers); // All member delete
+
+//kakao Routes
+protectedRouter.get("/kakao/auth", passport.authenticate("kakao-auth"));
+
+
 
 // Swagger endpoint
 protectedRouter.swagger({
